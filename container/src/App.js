4c69b5f64c82d9acb3017fdd4ba5b2c24ec481e5
itisaby <arnabbibhuti01@gmail.com>
2023-04-14 18:38:52 +0530

    first commit

---
+++ container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+
+function App() {
+  return (
+    <div>
+      HI There!
+      <hr />
+      <MarketingApp />
+    </div>
+  )
+}
+
+export default App

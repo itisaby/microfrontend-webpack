03d6bd4446e4512bbe1935311d2ccc2b214555be
itisaby <arnabbibhuti01@gmail.com>
2023-04-22 11:21:10 +0530

    onNavigate

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,19 +1,15 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
-  const history = useHistory();
 
   useEffect(() => {
-    // const { onParentNavigate } =
-     mount(ref.current),{
+    mount(ref.current, {
       onNavigate: () => {
-        console.log('container onNavigate')
-      }
-     }
-
+        console.log('The container noticed navigation in Marketing');
+      },
+    });
   });
 
   return <div ref={ref} />;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -3,28 +3,25 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory } from 'history';
 import App from './App';
 
-const mount = (el, {onNavigate}) => {
-    const history = createMemoryHistory();
-    history.listen(onNavigate);
-    // if (onNavigate) {
-    //     history.listen(onNavigate);
-    // }
-    ReactDOM.render(
-        <App history={history} />,
-        el
-    );
-    // return {
-    //     onParentNavigate({ pathname: nextPathname }) {
-    //         console.log('container', nextPathname);
-    //     }
-    // }
-}
+// Mount function to start up the app
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
 
+  history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, el);
+};
+
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-    const el = document.querySelector('#_marketing-dev-root');
-    if (el) {
-        mount(el, {});
-    }
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
